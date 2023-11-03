dd167606366729712470e87eb62606b8ada583c1
Victoria Valencia Velez <victoriavalenciave@gmail.com>
2023-11-02 19:40:22 -0500

    Se agrega el proyecto con la pantalla de inicio

---
+++ WPF-LoginForm/View/LoginView.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace WPF_LoginForm.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        ArrayList lista = new ArrayList();
+    
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void seq_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath;
+            string line;
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();            
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    lista.Add(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                label1.Content = "" + lista[20000];
+            }
+            
+        }
+
+
+    }
+}
