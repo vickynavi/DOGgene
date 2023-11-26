fcaa8a4ab0f4f21f53753a52ea806fe71545822c
pderekm <dpuerta@unal.edu.co>
2023-11-26 15:38:45 -0500

    Pantalla Resultados

---
+++ WPF-LoginForm/Resultados.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_LoginForm
+{
+    /// <summary>
+    /// Lógica de interacción para Resultados.xaml
+    /// </summary>
+    public partial class Resultados : Window
+    {
+        public Resultados()
+        {
+            InitializeComponent();
+        }
+    }
+}
